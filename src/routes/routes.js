20959a6a9d6e94daf157b74a0d91fcea6afd5dba
brazeen <117746210+brazeen@users.noreply.github.com>
2024-10-29 13:25:26 +0800

    added model+controller＋routes (#21)
    * chore: installed mssql, express, joi and body-parser libs, added dbConfig.js
    
    * added new tables
    
    * added school model js file
    
    * updated seedscript to work
    
    * moved dbconfig
    
    * updated package with azure/identity
    
    * inserted schools mock data and updated db design
    
    * added GET method in school.js
    
    * added school GET methods
    
    * changed getPrincipal method to user class
    
    * added carbonfootprint and energyusage M + C
    
    * added mock data for energyusage and carbonfootprint tables
    
    * added getSchoolbystudentID method
    
    * made model and controller + routes
    
    ---------
    
    Co-authored-by: brazeenNP <s10255790@connect.np.edu.sg>

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,12 @@
 const userRoute = require("./userRoute.js");
-
+const energyUsageRoute = require("./energyUsageRoute.js");
+const energyBreakdownRoute = require("./energyBreakdownRoute.js");
+const carbonFootprintRoute = require("./carbonFootprintRoute.js")
 const route = (app, upload) => {
     userRoute(app, upload);
+    energyUsageRoute(app, upload)
+    energyBreakdownRoute(app, upload)
+    carbonFootprintRoute(app, upload)
 };
 
 module.exports = route;
