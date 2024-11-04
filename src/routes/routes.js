be038ec74a45f257804d3454b82186f1271fa13b
jungsek <s10258726@connect.np.edu.sg>
2024-11-04 20:02:31 +0800

    Jung-Sek-Report (#40)
    * Report Generation
    
    * Remove .env from version control
    
    * change .gitignore & .env
    
    * Report saving
    
    * View Data
    
    * Recommendations generator
    
    * Predictions

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ const carbonFootprintRoute = require("./carbonFootprintRoute.js");
 const reportRoute = require("./reportRoute.js"); 
 const schoolRoute = require("./schoolRoute.js");
 const recommendationsRoute = require("./recommendationsRoute.js");
+const predictionsRoute = require("./predictionsRoute.js");
 
 const route = (app, upload) => {
     userRoute(app, upload);
@@ -14,6 +15,7 @@ const route = (app, upload) => {
     reportRoute(app, upload); 
     schoolRoute(app, upload);
     recommendationsRoute(app, upload);
+    predictionsRoute(app, upload);
 };
 
 module.exports = route;
