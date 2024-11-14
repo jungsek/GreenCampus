1b0052a21ab92db4e54b551d1ace1b65a90ece2e
brazeenNP <s10255790@connect.np.edu.sg>
2024-11-14 11:53:30 +0800

    change dashboard layout

--- src/public/scripts/dashboard.js
+++ src/public/scripts/dashboard.js
@@ -433,11 +433,11 @@ async function initImpactCard(placeholderYear) {
     }
 
     if (totalCarbonFootprintElement) {
-        totalCarbonFootprintElement.innerText = `${totalCarbonFootprint.toFixed(2)}`;
+        totalCarbonFootprintElement.innerText = `${totalCarbonFootprint.toFixed(2)} tonnes`;
     }
 
     if (totalEnergyUsageElement) {
-        totalEnergyUsageElement.innerText = `${totalEnergyUsage.toFixed(2)}`;
+        totalEnergyUsageElement.innerText = `${totalEnergyUsage.toFixed(2)} kWh`;
     }
 
     if (topContributorsElement) {
@@ -452,11 +452,11 @@ async function initImpactCard(placeholderYear) {
         } else if (Math.abs(carbonFootprintChange) == 0.0) {
             // No change case (near zero)
             carbonFootprintChangeElement.innerHTML = `<br><i class='bx bx-minus'></i> No change from last year`;
-            carbonFootprintChangeElement.style.color = "blue"; // Blue color for no change
+            carbonFootprintChangeElement.style.color = "black"; // Blue color for no change
         } else {
             // Set up icon and color based on increase or decrease
             const iconClass = carbonFootprintChange < 0 ? 'bx-trending-down' : 'bx-trending-up';
-            const color = carbonFootprintChange < 0 ? 'rgb(25, 176, 25)' : 'red';
+            const color = carbonFootprintChange < 0 ? 'rgb(37 99 235)' : 'rgb(220 38 38)';
             
             carbonFootprintChangeElement.innerHTML = `<br><i class='bx ${iconClass}'></i> ${Math.abs(carbonFootprintChange)}% from last year`;
             carbonFootprintChangeElement.style.color = color; // Apply color based on trend
@@ -475,7 +475,7 @@ async function initImpactCard(placeholderYear) {
         } else {
             // Set up icon and color based on increase or decrease
             const iconClass = energyUsageChange < 0 ? 'bx-trending-down' : 'bx-trending-up';
-            const color = energyUsageChange < 0 ? 'rgb(25, 176, 25)' : 'red';
+            const color = energyUsageChange < 0 ? 'rgb(37 99 235)' : 'rgb(220 38 38)';
             
             energyUsageChangeElement.innerHTML = `<br><i class='bx ${iconClass}'></i> ${Math.abs(energyUsageChange)}% from last year`;
             energyUsageChangeElement.style.color = color; // Apply color based on trend
