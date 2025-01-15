32417753bbadf8d0c22557907f11354f83ccee0a
brazeenNP <s10255790@connect.np.edu.sg>
2025-01-15 17:09:01 +0800

    carbonbreakdown and seed updates

--- src/routes/carbonBreakdownRoute.js
+++ src/routes/carbonBreakdownRoute.js
@@ -5,8 +5,8 @@ const authenticateToken = require("../middlewares/authenticateToken");
 const carbonBreakdownRoute = (app) => {
     app.get("/carbon-breakdowns", carbonBreakdownController.getAllCarbonBreakdowns); // Get all energy breakdowns
     app.get("/carbon-breakdowns/footprint/:energyUsageId", carbonBreakdownController.getCarbonBreakdownByFootprint); // Get breakdowns by energy usage ID
-    app.get("/energy-breakdowns/school/:schoolId", carbonBreakdownController.getCarbonBreakdownBySchool); // Get breakdowns by school
-    app.get("/energy-breakdowns/school/:schoolId/:year", carbonBreakdownController.getCarbonBreakdownPerYearBySchool); // Get breakdowns by schoo
+    app.get("/carbon-breakdowns/school/:schoolId", carbonBreakdownController.getCarbonBreakdownBySchool); // Get breakdowns by school
+    app.get("/carbon-breakdowns/school/:schoolId/:year", carbonBreakdownController.getCarbonBreakdownPerYearBySchool); // Get breakdowns by schoo
 };
 
 module.exports = carbonBreakdownRoute;
